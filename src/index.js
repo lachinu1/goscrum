360deb364b58ddc88668734737db08899194e2ff
Chinu <lachinu1@gmail.com>
2022-05-20 17:49:00 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { App } from './App'; // lo importamos como una constante
 
 // Redux
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
+import { App } from './App'; // lo importamos como una constante
 
 render(
   <React.StrictMode>
-    <HashRouter> {/* basename es para que las rutas aparezcan completas */}
+    <BrowserRouter basename="goscrum/"> 
       <Provider store={store}> 
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
